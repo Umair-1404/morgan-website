2c47d24da5ec68ad235e30f3edbb7f5fd6b169fb
Umair <saboowalaumair1404@gmail.com>
2024-11-09 23:03:20 +0530

    completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import "../App.css";
 
 function Footer(){
     return(
     <>
     <footer className="bg-dark">
-        <p className="p-4 m-0 ms-5 fs-5 text-white text-center">&copy; Copyright &copy;All Rights Reserved </p>
+        <p className="p-4 fs-5 text-white text-center footer">&copy; Copyright &copy;All Rights Reserved </p>
     </footer>
     </>
     );
